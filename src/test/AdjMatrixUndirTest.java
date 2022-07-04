427bcdff7e262d56cb263b56907b9917555f64ca
Ziocash <31541164+Ziocash@users.noreply.github.com>
2022-07-04 11:40:33 +0200

    Update AdjMatrixUndirTest.java
    [+] Added testTrueEquals to test equals method
    [+] Added testFalseEquals to test equals method

--- src/test/AdjMatrixUndirTest.java
+++ src/test/AdjMatrixUndirTest.java
@@ -8,27 +8,24 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AdjMatrixUndirTest
-{
+public class AdjMatrixUndirTest {
     AdjMatrixUndir matrixUndir = new AdjMatrixUndir();
 
     @Test
-    public void testAddVertex()
-    {
-        assertEquals(0,matrixUndir.addVertex("A"));
-        assertEquals(1,matrixUndir.addVertex("B"));
-        assertEquals(2,matrixUndir.addVertex("C"));
+    public void testAddVertex() {
+        assertEquals(0, matrixUndir.addVertex("A"));
+        assertEquals(1, matrixUndir.addVertex("B"));
+        assertEquals(2, matrixUndir.addVertex("C"));
     }
 
-    
     @Test
-    public void testContainsVertex()
-    {
-        assertEquals(0,matrixUndir.addVertex("A"));
-        assertEquals(1,matrixUndir.addVertex("B"));
-        assertEquals(2,matrixUndir.addVertex("C"));
+    public void testContainsVertex() {
+        assertEquals(0, matrixUndir.addVertex("A"));
+        assertEquals(1, matrixUndir.addVertex("B"));
+        assertEquals(2, matrixUndir.addVertex("C"));
         assertTrue(matrixUndir.containsVertex("A"));
         assertTrue(matrixUndir.containsVertex("B"));
         assertTrue(matrixUndir.containsVertex("C"));
@@ -36,11 +33,10 @@ public class AdjMatrixUndirTest
     }
 
     @Test
-    public void testEdgeMethods()
-    {
-        assertEquals(0,matrixUndir.addVertex("A"));
-        assertEquals(1,matrixUndir.addVertex("B"));
-        assertEquals(2,matrixUndir.addVertex("C"));
+    public void testEdgeMethods() {
+        assertEquals(0, matrixUndir.addVertex("A"));
+        assertEquals(1, matrixUndir.addVertex("B"));
+        assertEquals(2, matrixUndir.addVertex("C"));
         assertTrue(matrixUndir.containsVertex("A"));
         assertTrue(matrixUndir.containsVertex("B"));
         assertTrue(matrixUndir.containsVertex("C"));
@@ -49,6 +45,49 @@ public class AdjMatrixUndirTest
         matrixUndir.addEdge("A", "B");
         assertTrue(matrixUndir.containsEdge("A", "B"));
         assertTrue(matrixUndir.containsEdge("B", "A"));
-        assertThrows(IllegalArgumentException.class, () -> { matrixUndir.containsEdge("C", "D");});
+        assertThrows(IllegalArgumentException.class, () -> {
+            matrixUndir.containsEdge("C", "D");
+        });
+    }
+
+    @Test
+    public void testTrueEquals() {
+        AdjMatrixUndir tempEqualsMatrix = new AdjMatrixUndir();
+        assertEquals(0, matrixUndir.addVertex("A"));
+        assertEquals(0, tempEqualsMatrix.addVertex("A"));
+        assertEquals(1, matrixUndir.addVertex("B"));
+        assertEquals(1, tempEqualsMatrix.addVertex("B"));
+        assertEquals(2, matrixUndir.addVertex("C"));
+        assertEquals(2, tempEqualsMatrix.addVertex("C"));
+        assertEquals(3, matrixUndir.addVertex("D"));
+        assertEquals(3, tempEqualsMatrix.addVertex("D"));
+
+        matrixUndir.addEdge("A", "B");
+        tempEqualsMatrix.addEdge("A", "B");
+        matrixUndir.addEdge("C", "D");
+        tempEqualsMatrix.addEdge("C", "D");
+
+        assertTrue(matrixUndir.equals(tempEqualsMatrix));
+
+    }
+
+    @Test
+    public void testFalseEquals() {
+        AdjMatrixUndir tempEqualsMatrix = new AdjMatrixUndir();
+        assertEquals(0, matrixUndir.addVertex("A"));
+        assertEquals(0, tempEqualsMatrix.addVertex("A"));
+        assertEquals(1, matrixUndir.addVertex("B"));
+        assertEquals(1, tempEqualsMatrix.addVertex("B"));
+        assertEquals(2, matrixUndir.addVertex("C"));
+        assertEquals(2, tempEqualsMatrix.addVertex("C"));
+        assertEquals(3, matrixUndir.addVertex("D"));
+
+        matrixUndir.addEdge("A", "B");
+        tempEqualsMatrix.addEdge("A", "B");
+        matrixUndir.addEdge("A", "D");
+        tempEqualsMatrix.addEdge("C", "B");
+
+        assertFalse(matrixUndir.equals(tempEqualsMatrix));
+
     }
 }
