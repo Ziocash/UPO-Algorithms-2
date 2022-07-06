16bb36bfc02947569cad7f3abbae3de313f46ee2
Ziocash <31541164+Ziocash@users.noreply.github.com>
2022-07-06 22:19:17 +0200

    UnionFind implementation
    [+] Added UnionFind class

---
+++ src/test/UnionFindTest.java
@@ -0,0 +1,52 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.Test;
+
+import upo.union.UnionFind;
+
+public class UnionFindTest {
+    UnionFind unionFind = new UnionFind();
+
+    @Test
+    public void testMakeSetAndFind() {
+        unionFind.makeSet(1);
+        assertEquals(1, unionFind.find(1));
+        unionFind.makeSet(2);
+        assertEquals(2, unionFind.find(2));
+        unionFind.makeSet(3);
+        assertEquals(3, unionFind.find(3));
+    }
+
+    @Test
+    public void testUnionOnError() {
+        unionFind.makeSet(1);
+        unionFind.makeSet(1);
+        assertThrows(IllegalArgumentException.class,() -> unionFind.union(1, 1));
+    }
+
+    @Test
+    public void testAll(){
+        unionFind.makeSet(1);
+        unionFind.makeSet(2);
+        unionFind.union(1, 2);
+        assertEquals(1, unionFind.find(1));
+        assertEquals(1, unionFind.find(2));
+        unionFind.makeSet(3);
+        unionFind.makeSet(4);
+        unionFind.union(3, 4);
+        assertEquals(3, unionFind.find(3));
+        assertEquals(3, unionFind.find(4));
+        unionFind.makeSet(5);
+        unionFind.makeSet(6);
+        unionFind.union(5, 6);
+        assertEquals(5, unionFind.find(5));
+        assertEquals(5, unionFind.find(6));
+        unionFind.union(1, 3);
+        assertEquals(1, unionFind.find(4));
+        unionFind.union(5, 1);
+        assertEquals(5, unionFind.find(1));
+    }
+}
---
+++ src/upo/union/UnionFind.java
@@ -0,0 +1,79 @@
+package upo.union;
+
+import java.util.ArrayList;
+
+/**
+ * Develop a class that represents an unionfind operation abstraction.<br/>
+ * 
+ * Use integers as data.
+ */
+public class UnionFind {
+
+    private ArrayList<ArrayList<Integer>> collections;
+
+    public UnionFind() {
+        collections = new ArrayList<ArrayList<Integer>>();
+    }
+
+    private boolean exists(Integer value) {
+        for (var collection : collections)
+            if (collection.contains(value))
+                return true;
+        return false;
+    }
+
+    private int search(Integer value) {
+        for (var collection : collections)
+            if (collection.contains(value))
+                return collections.indexOf(collection);
+        return -1;
+    }
+
+    /**
+     * Creates a new collection with {@code root} as collection root
+     * 
+     * @param root Collection root
+     */
+    public void makeSet(Integer root) {
+        if (!exists(root)) {
+            var collection = new ArrayList<Integer>();
+            collection.add(0, root);
+            collections.add(collection);
+        }
+    }
+
+    /**
+     * Merges two collection in a single one, putting as root the root of the first
+     * collection
+     * 
+     * @param element1 Element in the first collection, it will be the new
+     *                 collection root
+     * @param element2 Element in the second collection
+     * @throws ScemoDiMerdaException
+     */
+    public void union(Integer element1, Integer element2) throws IllegalArgumentException {
+        int index1 = search(find(element1));
+        int index2 = search(find(element2));
+        if(index1 == index2)
+            throw new IllegalArgumentException("Cannot merge the same set to itself.");
+
+        if (index1 > -1 && index2 > -1) {
+            collections.get(index1).addAll(collections.get(index2));
+            collections.remove(collections.get(index2));
+        }
+    }
+
+    /**
+     * Returns the collection root in which the element is, otherwise -1
+     * 
+     * @param element the element to search for
+     * @return the collection root in which the element is, otherwise -1
+     */
+    public Integer find(Integer element) {
+        if (exists(element))
+            for (var collection : collections)
+                if (collection.contains(element))
+                    return collection.get(0);
+        return -1;
+    }
+}
